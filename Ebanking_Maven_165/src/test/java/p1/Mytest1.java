8e504ec695dc2d5f4cbe720b6cd5ce18442ad927
Administrator <Administrator@192.168.2.3>
2019-07-18 09:10:09 +0530

    FirstUpdation

---
+++ Ebanking_Maven_165/src/test/java/p1/Mytest1.java
@@ -0,0 +1,23 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class Mytest1 {
+  @Test
+  public void tc001() 
+  {
+	  
+  }
+  
+  @Test
+  public void tc002() 
+  {
+	  
+  }
+  
+  @Test
+  public void tc003() 
+  {
+	  
+  }
+}
---
+++ Ebanking_Maven_165/src/test/java/p2/Mytest2.java
@@ -0,0 +1,23 @@
+package p2;
+
+import org.testng.annotations.Test;
+
+public class Mytest2 {
+  @Test
+  public void tc004() 
+  {
+	  
+  }
+  
+  @Test
+  public void tc005() 
+  {
+	  
+  }
+  
+  @Test
+  public void tc006() 
+  {
+	  
+  }
+}
